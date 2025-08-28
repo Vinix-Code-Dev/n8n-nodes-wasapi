5430b1c64da0b46c640acd8ad911ae73ec567418
Juan Alvarez <juanalvarezzz.dev@gmail.com>
2025-08-28 15:38:08 -0500

    Add sendAttachment and sendMessage operations for WhatsApp integration, including properties and execution functions for sending files and messages. Implements display options and integrates with Wasapi SDK for seamless functionality.

---
+++ nodes/actions/whatsapp/sendAttachment.operation.ts
@@ -0,0 +1,54 @@
+import {
+    IDisplayOptions,
+	IExecuteFunctions,
+	INodeExecutionData,
+	INodeProperties,
+    updateDisplayOptions,
+} from 'n8n-workflow';
+import { commonProperties } from '../base/common.operation';
+import { executeCommon } from '../../helpers/executeCommon.helper';
+import { WasapiClient } from '@laiyon/wasapi-sdk';
+import { WhatsAppDTO } from '../../dto/WhatsAppDTO';
+import { ServiceFactory } from '../../factories/ServiceFactory';
+
+export const sendAttachmentProperties: INodeProperties[] = [
+
+			{
+				displayName: 'File Path',
+				name: 'filePath',
+				type: 'string',
+				default: '',
+				required: true,
+				description: 'URL or path of the file to send',
+			},
+			{
+				displayName: 'Caption',
+				name: 'caption',
+				type: 'string',
+				default: '',
+				required: false,
+				description: 'description of the file (optional)',
+			},
+			...commonProperties,
+		];
+ 
+
+        const displayOptions: IDisplayOptions = {
+            show: {
+                resource: ['whatsapp'],
+                operation: ['sendAttachment'],
+            },
+        };
+
+        export const sendAttachmentDescription = updateDisplayOptions(displayOptions, sendAttachmentProperties);
+    
+
+
+export async function executeSendAttachment(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
+		return await executeCommon.call(this, async (client: WasapiClient, item: any, i: number) => {
+			const whatsAppService = ServiceFactory.createWhatsAppService(client);
+			const attachmentData = WhatsAppDTO.attachmentFromExecuteFunctions(this, i);
+
+			return await whatsAppService.sendAttachment(attachmentData);
+		});
+	}
---
+++ nodes/actions/whatsapp/sendMessage.operation.ts
@@ -0,0 +1,45 @@
+import {
+    IDisplayOptions,
+    IExecuteFunctions,
+    INodeExecutionData,
+    INodeProperties,
+    updateDisplayOptions,
+} from 'n8n-workflow';
+
+import { commonProperties } from '../base/common.operation';
+import { executeCommon } from '../../helpers/executeCommon.helper';
+import { WasapiClient } from '@laiyon/wasapi-sdk';
+import { WhatsAppDTO } from '../../dto/WhatsAppDTO';
+import { ServiceFactory } from '../../factories/ServiceFactory';
+
+export const sendMessageProperties: INodeProperties[] = [
+
+    {
+        displayName: 'Message',
+        name: 'message',
+        type: 'string',
+        default: '',
+        required: true,
+        description: 'message to send',
+    },
+    ...commonProperties,
+];
+
+const displayOptions: IDisplayOptions = {
+    show: {
+        resource: ['whatsapp'],
+        operation: ['sendMessage'],
+    },
+};
+
+export const sendMessageDescription = updateDisplayOptions(displayOptions, sendMessageProperties);
+
+export async function executeSendMessage(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
+    return await executeCommon.call(this, async (client: WasapiClient, item: any, i: number) => {
+        const whatsAppService = ServiceFactory.createWhatsAppService(client);
+        const messageData = WhatsAppDTO.messageFromExecuteFunctions(this, i);
+
+        return await whatsAppService.sendMessage(messageData);
+    });
+}
+
