1940d3505d1bd8b4d5fe86ea9be6c6ecc9187396
Juan Alvarez <juanalvarezzz.dev@gmail.com>
2025-08-28 17:30:45 -0500

    Refactor update contact operation: Replace existing properties with WhatsApp number and integrate contact creation properties for streamlined contact updates.

--- nodes/actions/contact/updateContact.operation.ts
+++ nodes/actions/contact/updateContact.operation.ts
@@ -8,95 +8,18 @@ import {
 import { executeCommon } from '../../helpers/executeCommon.helper';
 import { ContactDTO } from '../../dto/ContactDTO';
 import { ServiceFactory } from '../../factories/ServiceFactory';
+import { contactCreateProperties } from './createContact.operation';
+
 export const updateContactProperties: INodeProperties[] = [
     {
-        displayName: 'Phone Number',
+        displayName: 'WhatsApp Number',
         required: true,
         name: 'wa_id',
         type: 'string',
         default: '',
         description: 'Remember that the phone number must have the country code and NO SPACES. (eg: 573102938401 instead of +57 310 293 8401).',
     },
-    {
-        displayName: 'First Name',
-        required: false,
-        name: 'first_name',
-        type: 'string',
-        default: '',
-        description: 'First name of the contact',
-    },
-    {
-        displayName: 'Last Name',
-        required: false,
-        name: 'last_name',
-        type: 'string',
-        default: '',
-        description: 'Last name of the contact',
-    },
-    {
-        displayName: 'Email Address',
-        required: false,
-        name: 'email',
-        type: 'string',
-        default: '',
-        description: 'Email address of the contact',
-    },
-    {
-        displayName: 'Notes',
-        required: false,
-        name: 'notes',
-        type: 'string',
-        default: '',
-        description: 'Notes of the contact',
-    },
-    {
-        displayName: 'Labels',
-        name: 'labels',
-        required: false,
-        type: 'multiOptions',
-        typeOptions: {
-            loadOptionsMethod: 'getLabels',
-        },
-        default: [],
-        description: 'Label of the contact',
-    },
-    {
-        displayName: 'Custom Fields',
-        name: 'custom_fields',
-        type: 'fixedCollection',
-        typeOptions: {
-            multipleValues: true,
-        },
-        default: {},
-        description: 'Custom fields to assign to the contact',
-        options: [
-            {
-                name: 'custom_fields',
-                displayName: 'Custom Fields',
-                values: [
-                    {
-                        displayName: 'Field Name',
-                        name: 'field_name',
-                        type: 'options',
-                        typeOptions: {
-                            loadOptionsMethod: 'getCustomFields',
-                        },
-                        default: '',
-                        description: 'Select the custom field',
-                        required: true,
-                    },
-                    {
-                        displayName: 'Field Value',
-                        name: 'field_value',
-                        type: 'string',
-                        default: '',
-                        description: 'Value for the custom field',
-                        required: true,
-                    },
-                ],
-            },
-        ],
-    },
+    ...contactCreateProperties
 ];
 
 const displayOptions: IDisplayOptions = {
@@ -112,7 +35,7 @@ export async function executeContactUpdate(this: IExecuteFunctions): Promise<INo
     return await executeCommon.call(this, async (client: any, item: any, i: number) => {
         const contactService = ServiceFactory.contactService(client);
         const wa_id = ContactDTO.getById(this, i);
-        const contactData = ContactDTO.update(this, i);
+        const contactData = ContactDTO.create(this, i);
         
         // Obtener y validar custom fields
         const customFieldsData = this.getNodeParameter('custom_fields', i) as any;
