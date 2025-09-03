8dbc40d383b5c8aebb5c79418b550b40c7b93677
Juan Alvarez <juanalvarezzz.dev@gmail.com>
2025-09-02 19:22:30 -0500

    Update display name and description for 'fromId' property in common operation to enhance clarity and user guidance.

--- nodes/actions/base/common.operation.ts
+++ nodes/actions/base/common.operation.ts
@@ -2,7 +2,7 @@ import { INodeProperties } from "n8n-workflow";
 
 export const commonProperties: INodeProperties[] = [
     {
-        displayName: 'Phone Wasapi',
+        displayName: 'Phone Wasapi Name or ID',
         name: 'fromId',
         type: 'options',
         typeOptions: {
@@ -10,7 +10,7 @@ export const commonProperties: INodeProperties[] = [
         },
         default: '',
         required: true,
-        description: 'Pick the phone number of your wasapi account',
+        description: 'Pick the phone number of your wasapi account. Choose from the list, or specify an ID using an <a href="https://docs.n8n.io/code/expressions/">expression</a>.',
     },
     {
         displayName: 'WhatsApp ID',
