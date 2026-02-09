1229fdc6c8a0bbe22cc95495ff3afbe010fa12d7
Juan Alvarez <juanalvarezzz.dev@gmail.com>
2026-02-09 17:05:48 -0500

    Remove Wasapi client and related services, DTOs, and helper functions for a cleaner codebase and improved maintainability.

--- nodes/services/UserService.ts
+++
@@ -1,9 +0,0 @@
-import { WasapiClient} from '../../wasapiClient';
-
-export class UserService {
-	constructor(private client: WasapiClient) {}
-
-	async getUser(): Promise<any> {
-		return await this.client.user.getUser();
-	}
-}
--- wasapiClient/modules/user.ts
+++
@@ -1,9 +0,0 @@
-import { IN8nHttpClient } from '../types';
-export class UserModule {
-    constructor(private client: IN8nHttpClient) {}
-
-    async getUser(): Promise<any> {
-        const response = await this.client.get('/user');
-        return response.data;
-    }
-}
